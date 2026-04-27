722371d645091893ae1f2150b2d04e3d5dad74af
RakibHassanSoft <rakibreings32@gmail.com>
2026-04-27 17:04:44 +0600

    initial commit

--- app/apify-development/components/ApifyServices.tsx
+++ app/apify-development/components/ApifyServices.tsx
@@ -6,13 +6,13 @@ import { ArrowRight } from "lucide-react";
 /* ---------------- REAL SVG ICONS ---------------- */
 
 const FacebookIcon = () => (
-  <svg viewBox="0 0 24 24" className="w-6 h-6 fill-[#1877F2]">
+  <svg viewBox="0 0 24 24" className="w-24 h-24 fill-[#1877F2]">
     <path d="M22 12a10 10 0 1 0-11.5 9.9v-7h-2.2V12h2.2v-2c0-2.2 1.3-3.4 3.3-3.4.9 0 1.8.1 2 .1v2.3h-1.4c-1.1 0-1.4.7-1.4 1.3v1.7h2.7l-.4 2.9h-2.3v7A10 10 0 0 0 22 12z" />
   </svg>
 );
 
 const GoogleIcon = () => (
-  <svg viewBox="0 0 24 24" className="w-6 h-6">
+  <svg viewBox="0 0 24 24" className="w-24 h-24">
     <path
       fill="#EA4335"
       d="M12 10.2v3.9h5.5c-.2 1.4-1.6 4-5.5 4a6.5 6.5 0 1 1 0-13 6 6 0 0 1 4.2 1.6l2.8-2.7A10 10 0 1 0 12 22c6 0 10-4 10-10 0-.7-.1-1.2-.2-1.8H12z"
@@ -21,7 +21,7 @@ const GoogleIcon = () => (
 );
 
 const MapsIcon = () => (
-  <svg viewBox="0 0 24 24" className="w-6 h-6">
+  <svg viewBox="0 0 24 24" className="w-24 h-24">
     <path
       fill="#34A853"
       d="M12 2C8.1 2 5 5.1 5 9c0 5.3 7 13 7 13s7-7.7 7-13c0-3.9-3.1-7-7-7zm0 9.5A2.5 2.5 0 1 1 12 6a2.5 2.5 0 0 1 0 5.5z"
@@ -30,13 +30,13 @@ const MapsIcon = () => (
 );
 
 const LinkedInIcon = () => (
-  <svg viewBox="0 0 24 24" className="w-6 h-6 fill-[#0A66C2]">
+  <svg viewBox="0 0 24 24" className="w-24 h-24 fill-[#0A66C2]">
     <path d="M20.45 20.45h-3.55v-5.6c0-1.34-.03-3.06-1.86-3.06-1.86 0-2.14 1.45-2.14 2.95v5.71H9.35V9h3.41v1.56h.05a3.74 3.74 0 0 1 3.37-1.85c3.6 0 4.27 2.37 4.27 5.46v6.28zM5.34 7.43a2.07 2.07 0 1 1 0-4.14 2.07 2.07 0 0 1 0 4.14zM7.12 20.45H3.56V9h3.56v11.45z" />
   </svg>
 );
 
 const ShopifyIcon = () => (
-  <svg viewBox="0 0 24 24" className="w-6 h-6 fill-[#7AB55C]">
+  <svg viewBox="0 0 24 24" className="w-24 h-24 fill-[#7AB55C]">
     <path d="M16 3.2c-.7-.2-1.5-.4-2.2-.4-2.5 0-4.4 1.6-5 4.1l-1 4.4-2 .5v12.6l10-2.2V3.2z" />
   </svg>
 );
